a4afa1a515e94e69d809bd681afc5b466c4e8be1
Jonathan Sick <jsick@lsst.org>
2018-07-16 16:19:30 -0500

    Set up nbreport package
    This is our typical setup.py, setup.cfg and setuptools_scm set up for
    PyPI packages.

---
+++ nbreport/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ('__version__')
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = 'unknown'
